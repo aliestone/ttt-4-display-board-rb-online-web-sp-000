c83ab63694b792203d3bf421283f6d301387cedf
Alie Stone <helloaliestone@gmail.com>
2020-09-03 20:56:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board = ["O", "X", "X", "X", "X", "X", "X", "X", "X"]
 def display_board
-    puts "   |   |   "
+    puts "board [0]   |   |   "
     puts "-----------"
     puts "   |   |   "
     puts "-----------"
